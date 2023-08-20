928e69aaa1fa99d4cd1db29727a5cf6d43d650b2
SyedAshrafAsif <45714259+SyedAshrafAsif@users.noreply.github.com>
2023-08-20 12:00:40 +0600

    Create VmCountry

---
+++ src/Libraries/Infrustructure/EmploymentProjectTeam02.Services.Model/VmCountry.cs
@@ -0,0 +1,15 @@
+﻿using EmploymentProjectTeam02.Shared.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmploymentProjectTeam02.Services.Model;
+
+public class VmCountry:IVm
+{
+    public int Id { get; set; }
+    public String? CountryName { get; set; }
+     
+}
